0eef196bb07c8d22c8a3ea7dc8b145e65558f31e
Alice Horsey <alicehorsey@gmail.com>
2020-12-07 14:38:23 +0000

    installed cors and set up in app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const app = express();
 const apiRouter = require("./routes/api_router")
+const cors = require("cors")
 const { send404, handleInternalErrors, handlePSQLErrors, handleCustomErrors } = require("./controllers/errors")
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', apiRouter);
 app.all('/*', send404);
+
 
 app.use(handlePSQLErrors)
 app.use(handleCustomErrors)
